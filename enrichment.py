385c019bfb86a9f4f8afc4e560e278c9877fef12
Erik Clarke <erikclarke@gmail.com>
2012-07-19 16:32:29 -0700

    compliance with pep8 style guides

--- enrichment.py
+++ enrichment.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os
 import json
 import multiprocessing
-import hashlib
 import time
 
 from multiprocessing import Process
-from collections import defaultdict
 from ConfigParser import ConfigParser
 from optparse import OptionParser
 from contextlib import closing
@@ -22,20 +19,25 @@ from Annotations import parse_flat
 
 
 # MySQL commands (reference results_db_schema.sql)
-set_isolation_level = """set session transaction isolation level read committed"""
+set_isolation_level = """set session transaction isolation level 
+read committed"""
 
 
 store_results_sql = """
-replace into {table} (ontology, goid, term, pval, dataset, factor, subset, year, num_annos, num_genes, anno_min, anno_max, min_depth, max_depth, min_var, filter_similar, filter_size, filter_depth, shuffled)
+replace into {table} (ontology, goid, term, pval, dataset, factor, subset, 
+    year, num_annos, num_genes, anno_min, anno_max, min_depth, max_depth, 
+    min_var, filter_similar, filter_size, filter_depth, shuffled)
  values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
 """
 
 select_pvals_sql = """
-select goid, pval from {table} where dataset=%s and subset=%s and year=%s and ontology=%s and shuffled=%s
+select goid, pval from {table} where dataset=%s and subset=%s and year=%s 
+and ontology=%s and shuffled=%s
 """
 
 insert_qval_sql = """
-update {table} set qval=%s where dataset=%s and subset=%s and year=%s and ontology=%s and shuffled=%s and goid=%s
+update {table} set qval=%s where dataset=%s and subset=%s and year=%s
+ and ontology=%s and shuffled=%s and goid=%s
 """
 
 
@@ -65,7 +67,8 @@ def filter_annos(annotations, _max, _min):
     for k, v in annotations.iteritems():
         if len(v['genes']) > _max or len(v['genes']) < _min:
             del returned[k]
-    print "Removed %d terms from annotation set." % (len(annotations) - len(returned))
+    print "Removed %d terms from annotation set." % (len(annotations) - 
+        len(returned))
     return returned
 
 
@@ -74,7 +77,8 @@ def filter_annos_by_depth(annotations, min_depth, max_depth):
     for k, v in annotations.iteritems():
         if len(v['parents']) < min_depth or len(v['parents']) > max_depth:
             del returned[k]
-    print "Removed %d terms from annotation set." % (len(annotations) - len(returned))
+    print "Removed %d terms from annotation set." % (len(annotations) -
+        len(returned))
     return returned
 
 
@@ -86,11 +90,12 @@ def filter_similar_terms(annotations, min_variance):
         for p in parents:
             if p in annotations:
                 pgenes = annotations[p]['genes']
-                if (len(pgenes)-len(genes)) < min_variance:
+                if (len(pgenes) - len(genes)) < min_variance:
                     if k in returned:
                         del returned[k]
                     continue
-    print "Removed %d terms from annotation set." % (len(annotations) - len(returned))
+    print "Removed %d terms from annotation set." % (len(annotations) -
+        len(returned))
     return returned
 
 
@@ -101,14 +106,17 @@ def restrict_subontology(annotation_dict, ontology, year):
     try:
         ontology = parse_flat("data/go-%s.flat" % year)
     except IOError:
-        print("Warning: Flattened ontology file not found for year: %s. Create flattened ontology using go_flattener.jar." % year)
-        print("No subontology restriction done; using all terms from all ontologies.")
+        print("Warning: Flattened ontology file not found for year: %s."
+            " Create flattened ontology using go_flattener.jar." % year)
+        print("No subontology restriction done; using all terms from all "
+            "ontologies.")
         return annotation_dict
     returndict = dict(annotation_dict)
     for term in annotation_dict:
         if not go_id in ontology[term]:
             del returndict[term]
-    print "Restricting to %s removed %d terms." % (name, (len(annotation_dict) - len(returndict)))
+    print "Restricting to %s removed %d terms." % (name, 
+        (len(annotation_dict) - len(returndict)))
     return returndict
 
 
@@ -126,23 +134,27 @@ def get_connection(max_retries=30):
             time.sleep(5)
             i += 1
     print("Max retries reached, aborting...")
-    raise mysql.OperationalError("Failed to connect after %d retries" % max_retries)
+    raise mysql.OperationalError("Failed to connect after %d retries" 
+        % max_retries)
 
 
 def store_in_db(fn):
-    def store(dataset, platform, factor, subset, annotations, year, shuffled, num_annos, ontology, u2emap):
-        results, diffexp = fn(dataset, platform, factor, subset, annotations, year, u2emap)
+    def store(dataset, platform, factor, subset, annotations,
+     year, shuffled, num_annos, ontology, u2emap):
+        results, diffexp = fn(dataset, platform, factor, subset, annotations,
+         year, u2emap)
         p = multiprocessing.current_process()
         db = get_connection(100)
         with closing(db.cursor()) as c:
             col_results = []
             for goid, pval in results.iteritems():
                 if pval == 1:
-                    continue # we don't need to store pvals of 1
-                _id = (dataset.id, factor, subset, year, ontology)
-                _subid =  _id+(goid,)
-                col_results.append((ontology, goid, annotations[goid]['name'], pval, dataset.id,
-                                    factor, subset, year, num_annos, len(diffexp), ANNO_MIN_SIZE, ANNO_MAX_SIZE, MIN_DEPTH, MAX_DEPTH, MIN_VARIANCE, FILTER_SIMILAR, FILTER_BY_SIZE, FILTER_BY_DEPTH, shuffled))
+                    continue    # we don't need to store pvals of 1
+                col_results.append((ontology, goid, annotations[goid]['name'], 
+                    pval, dataset.id, factor, subset, year, num_annos,
+                    len(diffexp), ANNO_MIN_SIZE, ANNO_MAX_SIZE, MIN_DEPTH,
+                    MAX_DEPTH, MIN_VARIANCE, FILTER_SIMILAR, FILTER_BY_SIZE,
+                    FILTER_BY_DEPTH, shuffled))
             assert '{table}' not in store_results_sql
             c.executemany(store_results_sql, col_results)
             db.commit()
@@ -170,13 +182,11 @@ def enriched(dataset, platform, factor, subset, annotations,
         if len(diffexp) == 0:
             pval = 1
         else:
-            pval = ea._fexact(diffexp, not_diffexp, background, annotations[term],
-                              uniprot2entrez_map)
+            pval = ea._fexact(diffexp, not_diffexp, background,
+                annotations[term], uniprot2entrez_map)
         if pval < QVAL_CUTOFF:
             print "<{name}>: ({i}/{total}) {pval}\t{term}".format(name=p.name,
-                                                                  i=i, pval=pval, 
-                                                                  term=annotations[term]['name'], 
-                                                                  total=total)
+                i=i, pval=pval, term=annotations[term]['name'], total=total)
         results[term] = pval
     return results, diffexp
 
@@ -187,19 +197,22 @@ def multitest_correction(dataset, ontology, annotation_files):
     db = get_connection(100)
     for annotations in annotation_years:
         year = annotations['meta']['year']
-        shuffled = annotations['meta']['shuffled'] if 'shuffled' in annotations['meta'] else 0.0
+        # If we didn't shuffle the annotations, the shuffle level is 0
+        shuffled = annotations['meta'].get('shuffled', 0.0)
         for subset in dataset.factors[factor]:
-            print "[%s]-[%s]-[%s]-[%s]-[%f]:" % (dataset.id, year, ontology, subset, shuffled),
+            print"[%s]-[%s]-[%s]-[%s]-[%f]:" % (dataset.id, year, 
+                ontology, subset, shuffled),
             with closing(db.cursor()) as c:
                 _id = (dataset.id, subset, year, ontology, shuffled)
                 print "selecting pvals... ",
                 c.execute(select_pvals_sql, _id)
-                results = list(c.fetchall())    # list of tuples [(_subid, pval), ...]
+                # list of tuples [(_subid, pval), ...]
+                results = list(c.fetchall())
             pvals = [x[1] for x in results] 
-            subids = [_id+(x[0],) for x in results]
+            subids = [_id + (x[0],) for x in results]
             print "calculating FDR... ",
             rejected, qvals = multitest.fdrcorrection(pvals)
-            results = [(qvals[i],)+subids[i] for i,v in enumerate(qvals)]
+            results = [(qvals[i],) + subids[i] for i, v in enumerate(qvals)]
             with closing(db.cursor()) as c:
                 print "inserting %d qvals... " % len(results),
                 c.executemany(insert_qval_sql, results)
@@ -225,12 +238,13 @@ def main(file_or_accn, annotation_files, ontology):
     # acquire the platform used from the dataset metadata
     platform = fetch(dataset.meta['platform'], destdir='data')
 
-    print("Detected %d cores, splitting into %d subprocesses..." % (NCORES, NCORES))
+    print("Detected %d cores, splitting into %d subprocesses..." 
+        % (NCORES, NCORES))
 
     if FDR_CORRECTION:
         jobs = []
         for annofile in annotation_files:
-            p= Process(target=multitest_correction,
+            p = Process(target=multitest_correction,
                        args=(dataset, ontology, [annofile]))
             jobs.append(p)
             p.start()
@@ -240,32 +254,38 @@ def main(file_or_accn, annotation_files, ontology):
     for annotations in annotation_years:
         year = annotations['meta']['year']
         annos = annotations['anno']
-        shuffled_amnt = annotations['meta']['shuffled'] if 'shuffled' in annotations['meta'] else 0
+        shuffled = annotations['meta'].get('shuffled', 0.0)
         if FILTER_SIMILAR:
-            print "Filtering out terms with less than a %d-gene difference from their parents" % MIN_VARIANCE
+            print("Filtering out terms with less than a %d-gene "
+                "difference from their parents" % MIN_VARIANCE)
             annos = filter_similar_terms(annos, MIN_VARIANCE)
         if FILTER_BY_DEPTH:
-            print "Filtering out terms with fewer than %d or greater than %d parents" % (MIN_DEPTH, MAX_DEPTH)
+            print("Filtering out terms with fewer than %d "
+                "or greater than %d parents" % (MIN_DEPTH, MAX_DEPTH))
             annos = filter_annos_by_depth(annos, MIN_DEPTH, MAX_DEPTH)
         if FILTER_BY_SIZE:
-            print "Filtering out annotation gene sets greater than %d and less than %d" % (ANNO_MAX_SIZE, ANNO_MIN_SIZE)
+            print("Filtering out annotation gene sets greater than %d "
+                "and less than %d" % (ANNO_MAX_SIZE, ANNO_MIN_SIZE))
             annos = filter_annos(annos, ANNO_MAX_SIZE, ANNO_MIN_SIZE)
         filtered_annotations = restrict_subontology(annos, ontology, year)
         blocks = split(filtered_annotations, blocks=NCORES)
-        print("Split %d annotations into %d blocks of ~%d terms each..." % (len(filtered_annotations), len(blocks), len(blocks[0])))
-        # We're actually going to only look at "disease state" factors for this analysis.
+        print("Split %d annotations into %d blocks of ~%d terms each..." 
+            % (len(filtered_annotations), len(blocks), len(blocks[0])))
+        # We're only looking at one factor for this analysis
+        # Iterate over factors if this is no longer true
         factor = 'disease state'
         for subset in dataset.factors[factor]:
-            print("-- [year: %s] [dataset: %s] [%s: %s] --" % (year, dataset.id, 
-                                                               factor, subset))
+            print("-- [year: %s] [dataset: %s] [%s: %s] --" 
+                % (year, dataset.id, factor, subset))
             jobs = []
             for block in blocks:
-                p = Process(target=enriched,
-                            args=(dataset, platform, factor, subset, 
-                                  block, year, shuffled_amnt, len(filtered_annotations), ontology, uniprot2entrez_map))
+                p = Process(target=enriched, 
+                    args=(dataset, platform, factor, subset, block, year, 
+                        shuffled, len(filtered_annotations), ontology, 
+                        uniprot2entrez_map))
                 jobs.append(p)
                 p.start()
-            [p.join() for p in jobs] # wait for them all to finish
+            [p.join() for p in jobs]  # wait for them all to finish
 
 
 def print_usage():
@@ -277,43 +297,66 @@ Specific options are defined in config files, which can be passed to the
 program with the --config <CFG_FILE> option. By default, the script looks for
 configs/settings.cfg."""
 
-    print(textwrap.fill(usage,79))
+    print(textwrap.fill(usage, 79))
     print("\nSee README.md for more detailed information.\n")
 
 if __name__ == '__main__':
-    parser = OptionParser(usage='%prog [options] <GEO dataset> <annotation file 1 [anno file 2...]>')
+    parser = OptionParser(
+        usage='%prog [options] <GEO dataset> <anno file 1 [anno file 2...]>')
     config = ConfigParser()
 
     if '--config' in sys.argv:
-        cfg_file = sys.argv[sys.argv.index('--config')+1]
+        cfg_file = sys.argv[sys.argv.index('--config') + 1]
         config.read(cfg_file)
     else:
         config.read("configs/settings.cfg")
 
-    parser.add_option('--config', action='store', dest='config', default='configs/settings.cfg', help="Alternate configuration file")
-    parser.add_option('-o', action='store', type='choice', choices=['MF','CC','BP'], dest='ontology', help="REQUIRED: GO sub-ontology to use (MF, CC, BP)")
-    parser.add_option('--fdr_correction', action='store_true', default=False, dest='fdrcorr', help="Calculate p-values instead of doing EA")
-    parser.add_option('--use_shuffled', action='store_true', default=False, dest='shuffled', help="Work with shuffled annotations")
-    parser.add_option('--filter_by_size', action='store_true', default=False, dest='filter_size', 
-                      help="Filter out term that have more or less than the specified max and min gene set sizes")
-    parser.add_option('--filter_by_depth', action='store_true', default=False, dest='filter_depth', 
-                      help="Filter terms by ontology depth (terms above/below a certain depth are omitted)") 
-    parser.add_option('--filter_similar', action='store_true', default=False, dest='filter_similar', 
-                      help="Filter out terms that have less than a certain number of genes not in common with their parents")
-    parser.add_option('--max_depth', action='store', type=int, dest='max_depth', 
-                      default=config.getint('Annotations', 'max depth'), help="Maximum ontology depth of terms")
-    parser.add_option('--min_depth', action='store', type=int, dest='min_depth', 
-                      default=config.getint('Annotations', 'min depth'), help="Minimum ontology depth of terms")
+    parser.add_option('--config', action='store', dest='config', 
+        default='configs/settings.cfg', help="Alternate configuration file")
+    parser.add_option('-o', action='store', type='choice', 
+        choices=['MF', 'CC', 'BP'], dest='ontology', 
+        help="REQUIRED: GO sub-ontology to use (MF, CC, BP)")
+    parser.add_option('--fdr_correction', action='store_true',
+        default=False, dest='fdrcorr', 
+        help="Calculate p-values instead of doing EA")
+    parser.add_option('--use_shuffled', action='store_true', 
+        default=False, dest='shuffled', help="Work with shuffled annotations")
+    parser.add_option('--filter_by_size', action='store_true', 
+        default=False, dest='filter_size', 
+        help=("Filter out term that have more or less than the specified max "
+            "and min gene set sizes"))
+    parser.add_option('--filter_by_depth', action='store_true', 
+        default=False, dest='filter_depth', 
+        help=("Filter terms by ontology depth (terms above/below a certain "
+            "depth are omitted)"))
+    parser.add_option('--filter_similar', action='store_true', 
+        default=False, dest='filter_similar',
+        help=("Filter out terms that have less than a certain number of genes "
+            "not in common with their parents"))
+    parser.add_option('--max_depth', action='store', type=int, 
+        dest='max_depth', default=config.getint('Annotations', 'max depth'), 
+        help="Maximum ontology depth of terms")
+    parser.add_option('--min_depth', action='store', type=int, 
+        dest='min_depth', default=config.getint('Annotations', 'min depth'), 
+        help="Minimum ontology depth of terms")
     parser.add_option('--max_size', action='store', type=int, dest='max_size', 
-                      default=config.getint('Annotations', 'max size'), help="Maximum number of genes annotated to term")
+        default=config.getint('Annotations', 'max size'), 
+        help="Maximum number of genes annotated to term")
     parser.add_option('--min_size', action='store', type=int, dest='min_size', 
-                      default=config.getint('Annotations', 'min size'), help="Minimum number of genes annotated to term")
-    parser.add_option('--min_var', action='store', type=int, dest='min_variance', 
-                      default=config.getint('Annotations', 'min variance'), help="Minimum number of genes a child term must have different from a parent")
+        default=config.getint('Annotations', 'min size'), 
+        help="Minimum number of genes annotated to term")
+    parser.add_option('--min_var', action='store', type=int, 
+        dest='min_variance', default=config.getint('Annotations', 
+            'min variance'), 
+        help=("Minimum number of genes a child term must have different from a"
+            " parent"))
     parser.add_option('--max_fdr', action='store', type=float, dest='max_fdr', 
-                      default=config.getfloat('FDR', 'cutoff'), help="FDR q-value cutoff for defining differentially expressed genes")
+        default=config.getfloat('FDR', 'cutoff'), 
+        help="FDR q-value cutoff for defining differentially expressed genes")
     parser.add_option('--sql_table', action='store', dest='sql_table', 
-                      default=config.get('MySQL', 'table'), help="Table to store results (other MySQL options specified in config file)")
+        default=config.get('MySQL', 'table'), 
+        help=("Table to store results (other MySQL options specified in "
+            "config file)"))
 
     opts, args = parser.parse_args()
 
@@ -362,11 +405,3 @@ if __name__ == '__main__':
     insert_qval_sql = insert_qval_sql.format(table=table)
 
     main(file_or_accn, annotation_files, ontology)
-
-                                     
-                    
-                    
-                                 
-                        
-
-                    
